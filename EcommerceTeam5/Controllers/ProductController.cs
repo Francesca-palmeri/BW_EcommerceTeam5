409d06c4d284292e0edb07385f17a3c6c252b95f
Francesca-palmeri <francesca.palmeri.c@gmail.com>
2025-03-03 15:43:17 +0100

    RISOLTI I PRIMI PROBLEMI (NE ARRIVERANNO ALTRI)

--- EcommerceTeam5/Controllers/ProductController.cs
+++ EcommerceTeam5/Controllers/ProductController.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using EcommerceTeam5.Models;
 using Microsoft.Data.SqlClient;
-using System;
 using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
@@ -11,19 +11,16 @@ namespace EcommerceTeam5.Controllers
 {
     [AutoValidateAntiforgeryToken]
     public class ProductController : Controller
-    { 
-        private readonly String _connectionString;
+    {
+        private readonly string _connectionString;
 
-        public ProductController() { 
-
-            var configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", false, true)
-                .Build();
-            
+        // Iniezione della configurazione tramite il costruttore
+        public ProductController(IConfiguration configuration)
+        {
             _connectionString = configuration.GetConnectionString("DefaultConnection");
         }
 
+        // Azione per visualizzare tutti i prodotti
         public async Task<IActionResult> Index()
         {
             var productsList = new Magazzino()
@@ -31,11 +28,12 @@ namespace EcommerceTeam5.Controllers
                 Products = new List<Product>()
             };
 
+            // Connessione al database per ottenere i prodotti
             await using (SqlConnection connection = new SqlConnection(_connectionString))
             {
                 await connection.OpenAsync();
-                string query = "SELECT Prodotti.prodottoId, Prodotti.Nome, Prodotti.Descrizione, Prodotti.Prezzo, Prodotti.ImmageURL, Prodotti.Creato, Prodotti.CategoriaId;";
-            
+                string query = "SELECT prodottoId, Nome, Descrizione, Prezzo, ImageURL, Creato, CategoriaId FROM Prodotti;";
+
                 await using (SqlCommand command = new SqlCommand(query, connection))
                 {
                     await using (SqlDataReader reader = await command.ExecuteReaderAsync())
@@ -50,16 +48,57 @@ namespace EcommerceTeam5.Controllers
                                 Prezzo = reader.GetDecimal(3),
                                 Immagine = reader.GetString(4),
                                 Creazione = reader.GetDateTime(5),
-                                Categoria = reader.GetString(6)
+                                Categoria = reader.GetInt32(6)
                             });
                         }
                     }
-                } 
+                }
             }
 
             return View(productsList);
         }
 
         [HttpGet("Product/{id}")]
+        public async Task<IActionResult> Details(int id)
+        {
+            Product product = null;
+
+           
+            await using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+                string query = "SELECT prodottoId, Nome, Descrizione, Prezzo, ImageURL, Creato, CategoriaId FROM Prodotti WHERE prodottoId = @Id;";
+
+                await using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", id);
+
+                    await using (SqlDataReader reader = await command.ExecuteReaderAsync())
+                    {
+                        if (await reader.ReadAsync())
+                        {
+                            product = new Product()
+                            {
+                                Id = reader.GetInt32(0),
+                                Nome = reader.GetString(1),
+                                Descrizione = reader.GetString(2),
+                                Prezzo = reader.GetDecimal(3),
+                                Immagine = reader.GetString(4),
+                                Creazione = reader.GetDateTime(5),
+                                Categoria = reader.GetInt32(6)
+                            };
+                        }
+                    }
+                }
+            }
+
+            
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
     }
 }
--- EcommerceTeam5/Models/Product.cs
+++ EcommerceTeam5/Models/Product.cs
@@ -3,13 +3,17 @@
     public class Product
     {
         public int Id { get; set; }
+
         public string? Nome { get; set; }
 
         public decimal Prezzo { get; set; }
-        public string? Descrizione { get; set; }
-        public string? Immagine { get; set; }
-        public string? Categoria { get; set; }
-        public DateTime Creazione { get; set; }
 
+        public string? Descrizione { get; set; }
+
+        public string? Immagine { get; set; }
+
+        public int Categoria { get; set; }  
+        
+        public DateTime Creazione { get; set; }
     }
 }
